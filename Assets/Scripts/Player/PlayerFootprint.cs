f571e01bab5c359400978c1880c204fe76b77974
jintg0821 <jintg0825@gmail.com>
2025-06-03 02:06:32 +0900

    [Fix]발자국 오브젝트 변경

--- Assets/Scripts/Player/PlayerFootprint.cs
+++ Assets/Scripts/Player/PlayerFootprint.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class PlayerFootprint : MonoBehaviour
 {
-    [SerializeField] private GameObject footprintPrefab;
+    [SerializeField] private GameObject leftFootprintPrefab;
+    [SerializeField] private GameObject rightFootprintPrefab;
     [SerializeField] private float footprintLifetime = 3f;
     [SerializeField] private Vector3 footprintOffset = new Vector3(0, 0.01f, 0);
 
@@ -11,13 +12,17 @@ public class PlayerFootprint : MonoBehaviour
         Vector3 spawnPos = footTransform.position + footprintOffset;
         Quaternion spawnRot = Quaternion.Euler(0, transform.eulerAngles.y, 0);
 
-        GameObject footprint = Instantiate(footprintPrefab, spawnPos, spawnRot);
-
         if (!isLeftFoot)
         {
-            footprint.transform.Rotate(0, 0, 180);
+            GameObject rightFootprint = Instantiate(rightFootprintPrefab, spawnPos, spawnRot);
+            rightFootprint.transform.Rotate(0, 180, 0);
+            Destroy(rightFootprint, footprintLifetime);
         }
-
-        Destroy(footprint, footprintLifetime);
+        else
+        {
+            GameObject leftFootprint = Instantiate(leftFootprintPrefab, spawnPos, spawnRot);
+            leftFootprint.transform.Rotate(0, 180, 0);
+            Destroy(leftFootprint, footprintLifetime);
+        }
     }
 }
